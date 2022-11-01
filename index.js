405fa0a4e1671dab05849dc01df962a5d80735c7
rajeshsiddabattina <97787724+rajeshsiddabattina@users.noreply.github.com>
2022-11-01 17:37:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('HELLO WELCOME TO AWS WORLD !')
+  res.send('HELLO WELCOME TO AWS WORLD!')
 })
 
 app.get('/status', (req, res) => {
