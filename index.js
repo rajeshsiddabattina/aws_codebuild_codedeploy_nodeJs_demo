a991dca616db29ef50e4ae93f9a90f9ff5f0dfcc
rajeshsiddabattina <97787724+rajeshsiddabattina@users.noreply.github.com>
2022-11-06 21:14:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('HELLO WELCOME TO AWS WORLD! vesrion 5')
+  res.send('HELLO WELCOME TO AWS ! vesrion 5')
 })
 
 app.get('/status', (req, res) => {
