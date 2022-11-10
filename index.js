dabeb1d848b1e4b5a67739a96e03691df5649dba
rajeshsiddabattina <97787724+rajeshsiddabattina@users.noreply.github.com>
2022-11-10 23:26:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('HELLO world | aws cicd ')
+  res.send('HELLO world | aws cicd v1 ')
 })
 
 app.get('/status', (req, res) => {
